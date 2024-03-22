5def274756ce52222ecb140e7b89579d305e3024
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-03-21 21:07:00 -0400

    modified title

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'FitBuddy',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'FitBuddy'),
     );
   }
 }
