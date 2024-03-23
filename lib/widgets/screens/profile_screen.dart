e8a915210b6fd30a4a9e7d4d06da5fef58b86607
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-03-23 19:51:17 -0400

    created profile screen

---
+++ lib/widgets/screens/profile_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:fitbuddy/widgets/screens/screen_manager.dart';
+
+class ProfileScreen extends ScreenManager {
+  const ProfileScreen({super.key})
+      : super(
+            body: const Center(
+              child: Text('Welcome to profile'),
+            ),
+            title: 'Profile');
+}
