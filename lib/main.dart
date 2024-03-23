3a0e1b9707424fa415bd37f2f1b28950d0020d57
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-03-23 15:39:51 -0400

    removed text buttons and cleaned up homepage class

--- lib/main.dart
+++ lib/main.dart
@@ -21,45 +21,27 @@ class FitBuddy extends StatelessWidget {
   }
 }
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   final String title;
 
   const HomePage({super.key, required this.title});
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  Widget build(BuildContext context) {
+    return const Positioned(
+      child: Text('Position'),
+    );
   }
+}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        // title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
+      body: const Text('HOMEPAGE'),
       bottomNavigationBar: const NavBar(text: 'Sample text'),
     );
   }
