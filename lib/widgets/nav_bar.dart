2ab3c7f9afdd381376f72c4b3670b1aa36c533e9
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-03-24 00:48:23 -0400

    added buttons

--- lib/widgets/nav_bar.dart
+++ lib/widgets/nav_bar.dart
@@ -3,12 +3,23 @@ import 'package:flutter/material.dart';
 class NavBar extends StatelessWidget {
   const NavBar({super.key});
 
+  void goHome() {
+    print('going home');
+  }
+
+  void goProfile() {
+    print('going to profile');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Row(
+    return Row(
       children: [
-        Expanded(child: Text('Home')),
-        Expanded(child: Text('Profile')),
+        Expanded(
+            child: TextButton(onPressed: goHome, child: const Text('Home'))),
+        Expanded(
+            child:
+                TextButton(onPressed: goProfile, child: const Text('Profile'))),
       ],
     );
   }
