694960a4c3cebc171a5bcb1d76432e0be05f64ce
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-03-24 13:13:36 -0400

    added full week to home screeen

--- lib/widgets/screens/home_screen.dart
+++ lib/widgets/screens/home_screen.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:fitbuddy/widgets/screens/screen_manager.dart';
+import 'package:fitbuddy/widgets/full_week.dart';
 
 class HomeScreen extends ScreenManager {
   const HomeScreen({super.key})
       : super(
-          body: const Center(
-            child: Text('Welcome to home'),
+          body: const Column(
+            children: [
+              FullWeek(),
+              Text('Welcome to home'),
+            ],
           ),
           title: 'Home',
         );
