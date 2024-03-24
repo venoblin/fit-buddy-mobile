422dc9abb50fac2ef2486b659549ec6de094b6a0
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-03-24 13:29:13 -0400

    added amount placeholder

--- lib/widgets/full_week.dart
+++ lib/widgets/full_week.dart
@@ -8,45 +8,66 @@ class FullWeek extends StatefulWidget {
 }
 
 class _FullWeekState extends State<FullWeek> {
+  List<int> dayWorkoutAmounts = [0, 0, 0, 0, 0, 0, 0];
+
   @override
   Widget build(BuildContext context) {
-    return const Row(
+    return Row(
       children: [
         Expanded(
           child: Column(
             children: [
-              Text('SUN'),
+              const Text('SUN'),
+              Text(dayWorkoutAmounts[0].toString()),
             ],
           ),
         ),
         Expanded(
           child: Column(
-            children: [Text('MON')],
+            children: [
+              const Text('MON'),
+              Text(dayWorkoutAmounts[1].toString()),
+            ],
           ),
         ),
         Expanded(
           child: Column(
-            children: [Text('TUE')],
+            children: [
+              const Text('TUE'),
+              Text(dayWorkoutAmounts[2].toString()),
+            ],
           ),
         ),
         Expanded(
           child: Column(
-            children: [Text('WED')],
+            children: [
+              const Text('WED'),
+              Text(dayWorkoutAmounts[3].toString()),
+            ],
           ),
         ),
         Expanded(
           child: Column(
-            children: [Text('THU')],
+            children: [
+              const Text('THU'),
+              Text(dayWorkoutAmounts[4].toString()),
+            ],
           ),
         ),
         Expanded(
           child: Column(
-            children: [Text('FRI')],
+            children: [
+              const Text('FRI'),
+              Text(dayWorkoutAmounts[5].toString()),
+            ],
           ),
         ),
         Expanded(
           child: Column(
-            children: [Text('SAT')],
+            children: [
+              const Text('SAT'),
+              Text(dayWorkoutAmounts[6].toString()),
+            ],
           ),
         ),
       ],
