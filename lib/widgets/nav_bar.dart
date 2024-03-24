9f926b00f2e84dd37b2bb82b5eee74f93d35670b
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-03-24 00:54:52 -0400

    renamed methods

--- lib/widgets/nav_bar.dart
+++ lib/widgets/nav_bar.dart
@@ -4,11 +4,11 @@ import 'package:fitbuddy/widgets/screens/profile_screen.dart';
 class NavBar extends StatelessWidget {
   const NavBar({super.key});
 
-  void goHome(BuildContext context) {
+  void goToHome(BuildContext context) {
     print('going home');
   }
 
-  void goProfile(BuildContext context) {
+  void goToProfile(BuildContext context) {
     print('going to profile');
     Navigator.push(
       context,
@@ -22,10 +22,10 @@ class NavBar extends StatelessWidget {
       children: [
         Expanded(
             child: TextButton(
-                onPressed: () => goHome(context), child: const Text('Home'))),
+                onPressed: () => goToHome(context), child: const Text('Home'))),
         Expanded(
             child: TextButton(
-                onPressed: () => goProfile(context),
+                onPressed: () => goToProfile(context),
                 child: const Text('Profile'))),
       ],
     );
