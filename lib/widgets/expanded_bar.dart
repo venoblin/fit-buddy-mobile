8fe27ca7475e155ad42b4cc3adc25a86801d590d
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-04-14 23:52:32 -0400

    worked on expanded bar

--- lib/widgets/expanded_bar.dart
+++ lib/widgets/expanded_bar.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
 class ExpandedBar extends StatelessWidget {
-  const ExpandedBar({super.key});
+  const ExpandedBar({super.key, required this.children});
+
+  final List<Widget> children;
+
+  List<Widget> createChildren() {
+    List<Widget> createdChildren = [];
+
+    return createdChildren;
+  }
 
   @override
   Widget build(BuildContext context) {
