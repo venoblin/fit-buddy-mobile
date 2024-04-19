f37db9739cfbb4373f1a1efcf7a470a9f13b3607
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-04-18 21:43:40 -0400

    added get from prefs

--- lib/services/preferences.dart
+++ lib/services/preferences.dart
@@ -8,3 +8,8 @@ Future<void> saveToPrefs(String key, String value) async {
   final prefs = await getPrefs();
   prefs.setString(key, value);
 }
+
+Future<String> getFromPrefs(String key) async {
+  final prefs = await getPrefs();
+  return prefs.getString(key) ?? '';
+}
