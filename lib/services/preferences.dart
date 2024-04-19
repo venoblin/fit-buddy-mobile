ab074dd236902870ce6ceb8079076f744944683d
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-04-18 21:49:55 -0400

    moved methods to their own class

--- lib/services/preferences.dart
+++ lib/services/preferences.dart
@@ -1,15 +1,17 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
-Future<SharedPreferences> getPrefs() async {
-  return await SharedPreferences.getInstance();
-}
+class Preferences {
+  static Future<SharedPreferences> _getPrefs() async {
+    return await SharedPreferences.getInstance();
+  }
 
-Future<void> saveToPrefs(String key, String value) async {
-  final prefs = await getPrefs();
-  prefs.setString(key, value);
-}
+  static Future<void> saveToPrefs(String key, String value) async {
+    final prefs = await _getPrefs();
+    prefs.setString(key, value);
+  }
 
-Future<String> getFromPrefs(String key) async {
-  final prefs = await getPrefs();
-  return prefs.getString(key) ?? '';
+  static Future<String> getFromPrefs(String key) async {
+    final prefs = await _getPrefs();
+    return prefs.getString(key) ?? '';
+  }
 }
