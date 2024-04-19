51ecf2a7e679f5a1ade14fba3c68a0f0604b1c2a
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2024-04-18 21:41:04 -0400

    added save to prefs method

--- lib/services/preferences.dart
+++ lib/services/preferences.dart
@@ -3,3 +3,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 Future<SharedPreferences> getPrefs() async {
   return await SharedPreferences.getInstance();
 }
+
+Future<void> saveToPrefs(String key, String value) async {
+  final prefs = await getPrefs();
+  prefs.setString(key, value);
+}
